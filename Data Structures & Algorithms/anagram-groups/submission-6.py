d5f310a9f158bbd3c18a3feee18d24c92ddb9deb
Laiba Murshid <laibamurshid1@gmail.com>
2026-06-25 22:56:16 -0500

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,46 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #create a empty hashmap
+        #loop thru list and add keys of different anagarms
+        #check if anagram exists
+        #if it does .append
+        #if doesnt create new key
+        #print
+        
+        group = {}
+
+        for word in strs:
+            key = tuple(sorted(word))
+            if key in group:
+                group[key].append(word)
+            else:
+                group[key] = [word]
+        return list(group.values())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        """
+        group = {} #empty hashmap
+
+        for word in strs:
+            key = tuple(sorted(word))
+            if key in group:
+                group[key].append(word)
+            else:
+                group[key] = [word]
+        return list(group.values())
+        
+        """
