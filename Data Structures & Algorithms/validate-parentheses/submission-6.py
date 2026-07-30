e881f7a4832783eb84d6f71f4db2fdaec601fa36
Laiba Murshid <laibamurshid1@gmail.com>
2026-07-30 15:17:41 -0500

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        #create an empty stack
+        #append each opening element into the stack
+        #create a while loop to go thru the string s
+        #if closing character is found, then see    end of may    
+        #if yes, then pop both chars
+        #if not return false
+
+        
+        stack = []
+        if len(s) <=1:
+            return False
+                    
+        for x in s:
+            if(x == '(' or x=='[' or x=='{'):
+                stack.append(x)
+            elif x==')':
+                if stack and stack.pop() == '(':
+                    pass
+                else:
+                    return False
+            elif x==']':
+                if stack and stack.pop()== '[':
+                    pass
+                else:
+                    return False
+            elif x=='}':
+                if stack and stack.pop() == '{':
+                    pass
+                else:
+                    return False
+
+        return len(stack)==0
